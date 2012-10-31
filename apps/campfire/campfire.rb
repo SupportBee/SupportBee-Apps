0056ceb386745c4da6f2c3f9bd44fcca6deba7dd
Avinasha Shastry <me@avinasha.com>
2012-10-31 19:18:30 +0700

    Minor edits

--- apps/campfire/campfire.rb
+++ apps/campfire/campfire.rb
@@ -1,9 +1,18 @@
 module Campfire
   module EventHandler
     def ticket_created
-      campfire = Tinder::Campfire.new settings.subdomain, :token => settings.token
-      room = campfire.find_room_by_name(settings.room)
-      room.speak "New Ticket: #{payload.ticket.subject}"
+      puts "##########{settings.inspect}"
+      return unless settings.notify_ticket_created == 1
+      #notify_ticket(payload.ticket)
+    end
+  end
+
+  module ActionHandler
+    def button
+      return [200, ''] unless payload.tickets
+      payload.tickets.each do |ticket|
+        notify_ticket(ticket)
+      end
     end
   end
 end
@@ -13,5 +22,14 @@ module Campfire
     string :subdomain, :required => true, :label => 'Subdomain'
     string :token, :required => true, :label => 'Token'
     string :room, :required => true, :label => 'Room'
+    boolean :notify_ticket_created, :default => true, :label => 'Notify when Ticket is created'
+
+    private 
+
+    def notify_ticket(ticket)
+      campfire = Tinder::Campfire.new settings.subdomain, :token => settings.token
+      room = campfire.find_room_by_name(settings.room)
+      room.speak "New Ticket: #{ticket.subject}"
+    end
   end
 end
