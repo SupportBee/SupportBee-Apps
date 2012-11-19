88e71fe8f5a4f387c30ed37d7fd75b8e1a3bbf02
Anh Do <dotuanhoanganh@gmail.com>
2012-11-19 20:50:07 +0700

    github app

---
+++ apps/github/github.rb
@@ -0,0 +1,37 @@
+module Github 
+  module ActionHandler
+    def button
+
+      begin
+        create_issue(payload.overlay.title, payload.overlay.description)
+      rescue Exception => e
+        return [500, e.message]
+      end
+
+      [200, "Ticket sent to Github Issues"]
+
+    end
+  end
+end
+
+
+
+module Github
+  require 'json'
+
+  class Base < SupportBeeApp::Base
+    string :owner, :required => true, :label => 'Owner'
+    string :repo, :required => true, :label => 'Repository'
+    string :token, :required => true, :label => 'Token'
+
+    private
+
+    def create_issue(issue_title, description)
+      response = http_post "https://api.github.com/repos/#{settings.owner}/#{settings.repo}/issues?access_token=#{settings.token}" do |req|
+        req.body = {:title => issue_title, :body => description}.to_json
+      end
+    end
+
+  end
+end
+
