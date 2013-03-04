63826d18752ccc8dd74ec5f6b5a1fc6688fc2b95
gb <bops.91@gmail.com>
2013-03-04 16:08:10 +0530

    Create kapsystem.rb

---
+++ apps/kapsystem.rb
@@ -0,0 +1,52 @@
+module Kapsystem
+  module EventHandler
+    def ticket_created
+      return true unless settings.notify_ticket_created.to_s == '1'
+      ticket = payload.ticket
+      notify_sms("New Ticket from #{ticket.requester.name || ticket.requester.email} - #{payload.ticket.subject}")
+    end
+
+    def customer_reply_created
+      puts "customer reply created"
+      puts settings
+      return true unless settings.notify_customer_reply_created.to_s == '1'
+      reply = payload.reply
+      notify_sms("New Reply from #{reply.replier.name || reply.replier.email} in #{payload.ticket.subject}")
+    end
+
+  end
+end
+
+module Kapsystem
+  module ActionHandler
+    def button
+     # Handle Action here
+     [200, "Success"]
+    end
+  end
+end
+
+module Kapsystem
+  class Base < SupportBeeApp::Base
+    string :username, :required => true, :label => 'Kapsystem Username', :hint => "Signup for a BulkSMS account at http://sms1.kapsystem.com/"
+    #password :password, :required => true, :label => 'Kapsystem Password'
+    #string :mobilenumber, :required => true, :label => 'Phone Number(s)', :hint => 'Ex: 44123123456,44123123457 (with country code, comma separated)'
+  #string :senderid, :required => true, :label => 'Sender ID'
+    #boolean :notify_ticket_created, :default => true, :label => 'Notify when a Ticket is created'
+    #boolean :notify_customer_reply_created, :default => true, :label => 'Notify when the Customer replies'
+
+
+    def notify_sms(message)
+      puts message
+      http_post "http://trans.kapsystem.com/web2sms.php" do |req|
+        req.params[:username] = settings.username
+        req.params[:password] = settings.password
+        req.params[:message] =  message[0...160]
+        req.params[:mobilenumber] = settings.mobilenumber 
+		req:params[:senderid] = settings.senderid
+      end
+    end
+
+  end
+end
+
