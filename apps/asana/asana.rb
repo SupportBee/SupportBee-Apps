2ea8be70d47beaaca4809a58d7104fb9fb7b67ab
Avinasha Shastry <me@avinasha.com>
2013-03-04 09:25:10 +0530

    Asana app works now

--- apps/asana/asana.rb
+++ apps/asana/asana.rb
@@ -1,11 +1,13 @@
 module Asana 
   module ActionHandler
     def button
-
+      http.basic_auth(settings.token, "")
       begin
         response = create_task(payload.overlay.title, payload.overlay.notes)
         return [500, response.body['errors'].first['message']] if response.body['errors'] and not(response.body['errors'].empty?)
       rescue Exception => e
+        puts e.message
+        puts e.backtrace
         return [500, e.message]
       end
 
@@ -16,19 +18,58 @@ module Asana
 end
 
 module Asana
+  class WorkspaceNotFound < ::StandardError
+    def message
+      "Cannot find Workspace"
+    end
+  end
+
+  class ProjectNotFound < ::StandardError
+    def message
+      "Cannot find Project"
+    end
+  end
+
   class Base < SupportBeeApp::Base
-    string :workspace_id, :required => true, :label => 'Workspace ID', :hint => 'Get Workspace ID by opening terminal and pasting the following command "curl -u <API-Token>: https://app.asana.com/api/1.0/workspaces"'
+    string :workspace, :required => true, :label => 'Workspace Name'
+    string :project, :required => true, :label => 'Project Name'
     string :token, :required => true, :label => 'Token'
 
     private
 
     def create_task(task_name, notes)
-      http.basic_auth(settings.token, "")
-      response = http_post "https://app.asana.com/api/1.0/tasks" do |req|
-        req.headers['Content-Type'] = 'application/json'
-        req.body = {:data => {:workspace => settings.workspace_id, :name => task_name, :notes => notes, :assignee => 'me'}}.to_json
-      end
+      workspace_id = fetch_workspace
+      task_id = add_task_to_workspace(workspace_id, task_name, notes)
+      project_id = fetch_project(workspace_id)
+      add_project_to_task(task_id, project_id)    
     end
 
+    def add_task_to_workspace(workspace_id, task_name, notes)
+      response = http_post "https://app.asana.com/api/1.0/tasks" do |req|
+        req.headers['Content-Type'] = 'application/json'
+        req.body = {:data => {:workspace => workspace_id, :name => task_name, :notes => notes, :assignee => 'me'}}.to_json
+      end
+      response.body['data']['id']
+    end
+
+    def fetch_workspace
+      response = http_get "https://app.asana.com/api/1.0/workspaces"
+      workspace = response.body['data'].select {|workspace| workspace['name'] == settings.workspace.strip}
+      raise WorkspaceNotFound if workspace.empty?
+      workspace.first['id']
+    end
+
+    def fetch_project(workspace_id)
+      response = http_get "https://app.asana.com/api/1.0/workspaces/#{workspace_id}/projects"
+      project = response.body['data'].select {|project| project['name'] == settings.project.strip}
+      raise ProjectNotFound if project.empty?
+      project.first['id']
+    end
+
+    def add_project_to_task(task_id, project_id)
+      http_post "https://app.asana.com/api/1.0/tasks/#{task_id}/addProject" do |req|
+        req.params[:project] = project_id
+      end
+    end
   end
 end
