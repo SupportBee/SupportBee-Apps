f70b7711d17ffc7010911ced50a888f6f53aeeda
BHOPANNA <gagan@ubuntu.ubuntu-domain>
2013-03-28 13:04:28 +0530

    added basecamp classic app

---
+++ apps/basecamp_classic/basecamp_classic.rb
@@ -0,0 +1,34 @@
+module BasecampClassic
+  module ActionHandler
+    def button
+      http.basic_auth(settings.api_token,"")
+
+      begin
+        create_message(payload.overlay.title, payload.overlay.description)
+      rescue Exception => e
+        return [500, e.message]
+      end
+
+      [200, "Ticket sent to Basecamp_classic"]
+
+    end
+  end
+end
+
+module BasecampClassic
+  class Base < SupportBeeApp::Base
+    string :api_token, :required => true, :label => 'Enter API token'
+    string :project_id, :required => true, :label => 'Enter Project ID'
+
+    private
+
+    def create_message(title, body)
+      response = http.post "https://wassupbee.basecamphq.com/projects/10954464/posts.json" do |req|
+        req.headers['Content-Type'] = 'application/json'
+        req.body = {post:{title:title, body:body}}.to_json
+      end
+    end
+
+  end
+end
+
