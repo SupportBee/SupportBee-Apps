0818cc3c182d7d0ad9e81210b5396c47abefb5f9
Avinasha Shastry <me@avinasha.com>
2013-04-10 11:10:43 +0530

    A working JIRA app

--- apps/jira/jira.rb
+++ apps/jira/jira.rb
@@ -4,13 +4,15 @@ module Jira
       http.basic_auth(settings.user_name, settings.password)
 
       begin
-        create_issue(payload.overlay.title, payload.overlay.description)
+        result = create_issue(payload.overlay.title, payload.overlay.description)
+        if result
+          return [200, "Ticket sent to JIRA"]
+        else
+          return [500, "Ticket not sent. Please check the settings of the app"]
+        end
       rescue Exception => e
         return [500, e.message]
       end
-
-      [200, "Ticket sent to Jira"]
-
     end
   end
 end
@@ -19,17 +21,18 @@ module Jira
   class Base < SupportBeeApp::Base
     string :user_name, :required => true, :label => 'Enter User Name'
     string :password, :required => true, :label => 'Enter Password'
-    string :account_name, :required => true, :label => 'Enter Account Name'
+    string :subdomain, :required => true, :label => 'Enter Subdomain'
     string :project_key, :required => true, :label => 'Enter Project Key'
     string :issue_type, :required => true, :label => 'Enter Issue Type'
 
     private
 
     def create_issue(summary, description)
-      response = http_post "https://#{settings.account_name}.atlassian.net/rest/api/2/issue" do |req|
-	req.headers['Content-Type'] = 'application/json'
+      response = http_post "https://#{settings.subdomain}.atlassian.net/rest/api/2/issue" do |req|
+	      req.headers['Content-Type'] = 'application/json'
         req.body = {fields:{project:{key:settings.project_key}, summary:summary, description:description, issuetype:{name:settings.issue_type}}}.to_json
       end
+      response.status == 201 ? true : false
     end
   end
 end
