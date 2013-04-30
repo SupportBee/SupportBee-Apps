f562741a0022e1caff95a9f5fb5afd15fc7ba763
BHOPANNA <gagan@ubuntu.ubuntu-domain>
2013-04-30 15:02:36 +0530

    refactored and added error handling to bigcommerce.rb

--- apps/bigcommerce/bigcommerce.rb
+++ apps/bigcommerce/bigcommerce.rb
@@ -1,16 +1,17 @@
 module Bigcommerce
   module EventHandler
-    # Handle 'ticket.created' event
     def ticket_created
       ticket = payload.ticket
       requester = ticket.requester
+      http.basic_auth(settings.username, settings.api_token)
 
       begin
         api = connect_to_bigcommerce
         
         if api
-          order = get_orders(api)
-          html = order_info_html(order)
+          orders = get_orders(api)
+          html = order_info_html(orders)
+          sent_note_to_customer(ticket, orders)
         else
           return [500, "Ticket not sent. Unable to connect to Bigcommerce"]
         end
@@ -22,15 +23,6 @@ module Bigcommerce
 
       comment_on_ticket(ticket, html)
       [200, "Ticket sent to Bigcommerce"]
-    end
-  end
-end
-
-module Bigcommerce
-  module ActionHandler
-    def button
-     # Handle Action here
-     [200, "Success"]
     end
   end
 end
@@ -50,31 +42,72 @@ module Bigcommerce
     end
 
     def get_orders(api)
-      orders = api.get_orders.last
+      begin
+        orders = api.get_orders
+      rescue 
+        puts "#{e.message}\n#{e.backtrace}"
+      end
     end
 
-    def order_info_html(order)
+    def sent_note_to_customer(ticket, orders)
+      order = orders.last
+      http.put "https://store-fjjj0va0.mybigcommerce.com/api/v2/customers/#{order['customer_id']}.json" do |req|
+        req.headers['Content-Type'] = "application/json"
+        req.body= {notes:generate_note(ticket)}.to_json
+      end
+    end
+
+    def generate_note(ticket)
+      "[Support Ticket] https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}"
+    end
+
+    def order_info_html(orders)
+      order = orders.last
+      order_items = get_ordered_items(order)
+      items_html = order_items_html(order_items)
+      date = DateTime.parse(order['date_created'])
+      formatted_date = date.strftime('%a %b %d %H:%M:%S')
       html = ""
-      html << "Order Details"
-      html << "<br/>Status:"
-      html << "<br/>#{order['status']}"
-      html << "<br/>Subtotal:"
-      html << "<br/>#{order['subtotal_inc_tax']}"
-      html << "<br/>Total:"
-      html << "<br/>#{order['total_inc_tax']}"
-      html << "<br/>Date Created:"
-      html << "<br/>#{order['date_created']}"
-      html << "<br/>" 
-      html << "<br/>"
-      html << "Order Link"
-      html << "<br/>"
-      html << order_info_link(order)
+      html << "<h3>Order Details"
+      html << "<p>Order Count: #{orders.count}"
+      html << "<p>Ordered Items:"
+      html << items_html
+      html << "<p><p><table>"
+      html << "<tr><th><h5>Status:"
+      html << "</th><th><h5>Subtotal:"
+      html << "</th><th><h5>Total:"
+      html << "</th><th><h5>Date Created:"
+      html << "</th></tr><tr><td>#{order['status']}"
+      html << "</td><td>#{order['subtotal_inc_tax'].to_f}"
+      html << "</td><td>#{order['total_inc_tax'].to_f}"
+      html << "</td><td>#{formatted_date}"
+      html << "</td></tr>"
+      html << "</table>"
+      html << ">>" + order_info_link(order)
       html
     end
 
     def order_info_link(order)
       url = settings.shop_url.split("/api").first
       "<a href='#{url}/admin/index.php?ToDo=viewOrder&orderId=#{order['id']}'>View Order Info</a>"
+    end
+
+    def get_ordered_items(order)
+      url = order['products']['url']
+      response = http.get "#{url}" do |req|
+        req.params['Accept'] = "application/json"
+      end
+      order_items = response.body if response
+    end
+
+    def order_items_html(order_items)
+      html = ""
+      if order_items.kind_of?(Array)
+        order_items.select{|item| html << "<br/><h5 style=\"font-weight:normal\">#{item['name']}"} 
+      else 
+        html << "<br/><h5 style=\"font-weight:normal\">#{order_items['name']}" if order_items
+      end
+      return html
     end
   
     def comment_on_ticket(ticket, html)
