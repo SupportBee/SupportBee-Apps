c82307f3bed8df34c1ce1cd4401e4b7dd9a27e8c
BHOPANNA <gagan@ubuntu.ubuntu-domain>
2013-05-21 12:08:24 +0530

    can pass multiple comma-separated user keys in the settings

--- apps/push_over/push_over.rb
+++ apps/push_over/push_over.rb
@@ -21,39 +21,48 @@ end
 module PushOver
   class Base < SupportBeeApp::Base
     # Settings
-    string :user_key, :required => true, :label => 'User Key from PushOver', :hint => 'Get your User Key by signing up at http://www.pushover.net/'
+    string :user_key, :required => true, :label => 'User Key from PushOver', :hint => 'Get your User Key by signing up at http://www.pushover.net/. You can include multiple users too. Add the user keys separated by commas.'
     boolean :send_customer_replies, :default => false, :label => 'Notify replies from Customers?'
     boolean :send_agent_replies, :default => false, :label => 'Notify replies from Agents?'
 
 
     def push_ticket(ticket)
-      http_post "https://api.pushover.net/1/messages.json" do |req|
-        req.params[:token] = OMNIAUTH_CONFIG['push_over']['apikey']
-        req.params[:user] = settings.user_key
-        req.params[:title] =  "#{ticket.subject} from #{ticket.requester.name} (#{ticket.requester.email})"
-        req.params[:message] = ticket.content.text[0..512] #512 Limitation from PushOver API
-        req.params[:url] = "https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}"
-      end
+      user_keys = settings.user_key.split(",")
+      user_keys.each{|user_key|
+        http_post "https://api.pushover.net/1/messages.json" do |req|
+          req.params[:token] = OMNIAUTH_CONFIG['push_over']['apikey']
+          req.params[:user] = user_key
+          req.params[:title] =  "#{ticket.subject} from #{ticket.requester.name} (#{ticket.requester.email})"
+          req.params[:message] = ticket.content.text[0..512] #512 Limitation from PushOver API
+          req.params[:url] = "https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}"
+        end
+      }
     end
 
     def push_customer_reply(reply, ticket)
-      http_post "https://api.pushover.net/1/messages.json" do |req|
-        req.params[:token] = OMNIAUTH_CONFIG['push_over']['apikey']
-        req.params[:user] = settings.user_key
-        req.params[:title] =  "RE: #{ticket.subject} from Customer #{reply.replier.name} (#{reply.replier.email})"
-        req.params[:message] = ticket.content.text[0..512]
-        req.params[:url] = "https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}"
-      end
+      user_keys = settings.user_key.split(",")
+      user_keys.each{|user_key|
+        http_post "https://api.pushover.net/1/messages.json" do |req|
+          req.params[:token] = OMNIAUTH_CONFIG['push_over']['apikey']
+          req.params[:user] = user_key
+          req.params[:title] =  "RE: #{ticket.subject} from Customer #{reply.replier.name} (#{reply.replier.email})"
+          req.params[:message] = ticket.content.text[0..512]
+          req.params[:url] = "https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}"
+        end 
+      }
     end
 
     def push_agent_reply(reply, ticket)
-      http_post "https://api.pushover.net/1/messages.json" do |req|
-        req.params[:token] = OMNIAUTH_CONFIG['push_over']['apikey']
-        req.params[:user] = settings.user_key
-        req.params[:title] =  "RE: #{ticket.subject} from Agent #{reply.replier.name} (#{reply.replier.email})"
-        req.params[:message] = ticket.content.text[0..10000]
-        req.params[:url] = "https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}"
-      end
+      user_keys = settings.user_key.split(",")
+      user_keys.each{|user_key|
+        http_post "https://api.pushover.net/1/messages.json" do |req|
+          req.params[:token] = OMNIAUTH_CONFIG['push_over']['apikey']
+          req.params[:user] = user_key
+          req.params[:title] =  "RE: #{ticket.subject} from Agent #{reply.replier.name} (#{reply.replier.email})"
+          req.params[:message] = ticket.content.text[0..10000]
+          req.params[:url] = "https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}"
+        end
+      }
     end
   end
 end
