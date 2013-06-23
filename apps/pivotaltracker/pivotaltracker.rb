b28584daa372d5d78fca3cb7131167488b9242b8
Gagan Bopanna <bops.91@gmail.com>
2013-06-23 19:37:16 +0530

    added error handling to the app

--- apps/pivotaltracker/pivotaltracker.rb
+++ apps/pivotaltracker/pivotaltracker.rb
@@ -8,8 +8,6 @@ module Pivotaltracker
         return [500, e.message]
       end
 
-      [200, "Ticket sent to Pivotal Tracker"]
-
     end
   end
 end
@@ -26,9 +24,16 @@ module Pivotaltracker
     def create_story(story_name, description)
       response = http_post "https://www.pivotaltracker.com/services/v3/projects/#{settings.project_id}/stories" do |req|
         req.headers['X-TrackerToken'] = settings.token
-        req.headers['Content-Type'] = 'application/xml'
-        req.body = "<story><story_type>feature</story_type><name>#{story_name}</name><description>#{description}</description></story>"
+        req.headers['Content-Type'] = 'application/json'
+        req.body = {story:{story_type:"feature", name:story_name, description:description}}.to_json
       end
+   
+      if response.status == 200
+        result = [200, "Ticket sent to Pivotal Tracker"] if response.status == 200
+      elsif response.status == 401
+        result = [500, "Unauthorized. Please check the Project ID and Token"]
+      end
+     
     end
 
   end
