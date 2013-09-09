06141b40161c4017123dbcd0bb3cef2ddf74d9de
Prateek Dayal <prateek@supportbee.com>
2013-09-09 12:10:08 +0700

    Audit Trail App

---
+++ apps/audit_trail/audit_trail.rb
@@ -0,0 +1,55 @@
+module AuditTrail
+  module EventHandler
+    
+    def ticket_archived
+      log_string(:action_type => 'Archived')
+    end
+
+    def ticket_unarchived
+      log_string(:action_type => 'Unarchived')
+    end
+
+    def ticket_spammed
+      log_string(:action_type => 'Spammed')
+    end
+    
+    def ticket_unspammed
+      log_string(:action_type => 'Unspammed')
+    end
+
+
+    def ticket_trashed
+      log_string(:action_type => 'Trashed')
+    end
+    
+    def ticket_untrashed
+      log_string(:action_type => 'Untrashed')
+    end
+
+  end
+end
+
+module AuditTrail
+  class Base < SupportBeeApp::Base
+    # Define Settings
+    # string :name, :required => true, :hint => 'Tell me your name'
+    # string :username, :required => true, :label => 'User Name'
+    # password :password, :required => true
+    # boolean :notify_me, :default => true, :label => 'Notify Me'
+
+    # White list settings for logging
+    # white_list :name, :username
+
+    # Define public and private methods here which will be available
+    # in the EventHandler and ActionHandler modules
+    def log_string(options={})
+      message = options[:message] || "#{options[:action_type]}"
+      puts payload.inspect
+      agent = payload.agent
+      message << " by #{agent.name} (#{agent.email})" if agent
+      message << " at #{Time.now.strftime("%I:%M %P, %D")} UTC"
+      payload.ticket.comment :html => message
+    end
+  end
+end
+
