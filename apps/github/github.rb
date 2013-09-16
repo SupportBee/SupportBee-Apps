df6a0b642420df1d06e211e65862995a38490743
Imranshaikh <imran@adshub.net>
2013-09-16 17:03:48 +0530

    Posting github url as a comment

--- apps/github/github.rb
+++ apps/github/github.rb
@@ -1,13 +1,15 @@
 module Github 
   module ActionHandler
     def button
-
+      ticket = payload.tickets.first
       begin
-        create_issue(payload.overlay.title, payload.overlay.description)
+        response = create_issue(payload.overlay.title, payload.overlay.description)
       rescue Exception => e
         return [500, e.message]
       end
 
+      html = issue_url(response)
+      comment_on_ticket(ticket, html)
       [200, "Ticket sent to Github Issues"]
 
     end
@@ -29,6 +31,17 @@ module Github
       response = http_post "https://api.github.com/repos/#{settings.owner}/#{settings.repo}/issues?access_token=#{token}" do |req|
         req.body = {:title => issue_title, :body => description}.to_json
       end
+    end
+
+    def issue_url(response)
+      html = ""
+      html << "Github Issue created!\n"
+      html << "<a href=#{response.body['html_url']}>#{response.body['title']}</a>"
+      html
+    end
+
+    def comment_on_ticket(ticket, html)
+      ticket.comment(:html => html)
     end
 
   end
