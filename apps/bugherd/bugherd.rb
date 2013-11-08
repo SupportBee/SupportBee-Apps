e589b09b1c0aded6959090bb51f33ae3bb0d4f02
Imranshaikh <imran@adshub.net>
2013-11-08 10:55:01 +0530

    Bugherd authentication code refactored

--- apps/bugherd/bugherd.rb
+++ apps/bugherd/bugherd.rb
@@ -1,11 +1,16 @@
 module Bugherd
   module ActionHandler
     def button
-      ticket = payload.ticket.first
-      task = create_task(payload.overlay.description)
-      html = task_info_html(ticket, task)
+      http_basic_auth(settings.token, "")
+      begin
+        ticket = payload.ticket.first
+        task = create_task(payload.overlay.description)
+        html = task_info_html(ticket, task)
+        comment_on_ticket(ticket, html)
+      rescue Exception => e
+        return [500, e.message]
+      end
 
-      comment_on_ticket(ticket, html)
       [200, "New Task created successfully in BugHerd"]
     end
   end
@@ -13,9 +18,8 @@ end
 
 module Bugherd
   require 'json'
-  
   class Base < SupportBeeApp::Base
-    string :api, :required => true, :label => 'Bugherd Api Key', :hint => 'Login to your Bugherd account, go to Settings > General Settings'
+    string :token, :required => true, :label => 'Bugherd Api Key', :hint => 'Login to your Bugherd account, go to Settings > General Settings'
     string :project, :required => true, :label => 'Project ID' 
 
     def create_task(description)
@@ -29,11 +33,12 @@ module Bugherd
             "status" => "backlog"
           }  
         }.to_json
-      end      
+      end 
+      response.body    
     end
 
     def task_info_html(ticket, task)
-      "Bugherd Task Created!\n <a href=''>#{ticket.subject}</a>"
+      "Bugherd Task Created!\n <a href='#{task['task']['admin_link']}'>#{ticket.subject}</a>"
     end
     
     def comment_on_ticket(ticket, html)
