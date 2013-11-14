454e07af87e922c725b285ac1c1252228bbdd856
Nisanth Chunduru <nisanth074@gmail.com>
2013-11-14 21:12:51 +0530

    Add batchbook app boilerplate

---
+++ apps/batchbook/batchbook.rb
@@ -0,0 +1,39 @@
+module Batchbook
+  module EventHandler
+    # Handle 'ticket.created' event
+    def ticket_created
+      return true
+    end
+
+    # Handle all events
+    def all_events
+      return true
+    end
+  end
+end
+
+module Batchbook
+  module ActionHandler
+    def button
+     # Handle Action here
+     [200, "Success"]
+    end
+  end
+end
+
+module Batchbook
+  class Base < SupportBeeApp::Base
+    # Define Settings
+    # string :name, :required => true, :hint => 'Tell me your name'
+    # string :username, :required => true, :label => 'User Name'
+    # password :password, :required => true
+    # boolean :notify_me, :default => true, :label => 'Notify Me'
+
+    # White list settings for logging
+    # white_list :name, :username
+
+    # Define public and private methods here which will be available
+    # in the EventHandler and ActionHandler modules
+  end
+end
+
