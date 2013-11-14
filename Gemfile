8b6345c329feb74d4b247f278e773b246352db6a
Nisanth Chunduru <nisanth074@gmail.com>
2013-11-15 03:09:30 +0530

    Find person on ticket.created

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'flowdock'
 gem 'bigcommerce', "~> 0.8.2"
 gem 'rubyzoho'
 gem 'restforce'
+gem 'httparty'
 
 gem 'guard-shotgun', :git =>'git://github.com/rchampourlier/guard-shotgun.git', :group => :development
 gem 'execjs', :group => :development
--- apps/batchbook/batchbook.rb
+++ apps/batchbook/batchbook.rb
@@ -2,7 +2,10 @@ module Batchbook
   module EventHandler
     # Handle 'ticket.created' event
     def ticket_created
-      return true
+      setup_batchbook
+      ticket = payload.ticket
+      person = find_person(ticket.requester)
+      true
     end
 
     # Handle all events
@@ -28,5 +31,21 @@ module Batchbook
     string :subdomain, required: true, label: 'Batchbook Subdomain'
     boolean :should_create_person, label: 'Create a New Person in Batchbook if one does not exist'
     boolean :return_ticket_content, label: 'Send ticket content to Batchbook'
+
+    include HTTParty
+
+    def setup_batchbook
+      self.class.base_uri("https://#{settings.subdomain}.batchbook.com")
+    end
+
+    def find_person(requester)
+      options = { query: default_query_options.merge({ email: requester.email }) }
+      response = self.class.get('/api/v1/people.json', options)
+      response.parsed_response['people'].first
+    end
+
+    def default_query_options
+      { auth_token: settings.auth_token }
+    end
   end
 end
