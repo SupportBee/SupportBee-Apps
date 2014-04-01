316303a03b6d71e81a6003bc65ca8f236a1c99ec
subin <subinthattaparambil@gmail.com>
2014-04-02 00:20:58 +0530

    comments added

--- apps/slack/slack.rb
+++ apps/slack/slack.rb
@@ -3,30 +3,37 @@ module Slack
   module EventHandler
 
    def ticket_created
+      return unless settings.notify_ticket_created.to_s == '1'
       post_ticket(payload.ticket)
       true
     end
 
     def agent_reply_created
+      return unless settings.notify_agent_reply_created.to_s == '1'
       post_reply(payload.reply, payload.ticket)
       true
     end
 
     def customer_reply_created
+      return unless settings.notify_customer_reply_created.to_s == '1'
       post_reply(payload.reply, payload.ticket)
       true
     end
+
+    def comment_created
+      return unless settings.notify_comment_created.to_s == '1'      
+      post_comment(payload.comment)
+      true      
+    end    
 
   end
 
 end
 
 module Slack
-
   module ActionHandler
 
     def button     
-     # Handle Action here
      [200, "Success"]
     end
 
@@ -40,9 +47,16 @@ module Slack
 
   class Base < SupportBeeApp::Base
     
-    string :token, :required => true, :hint => 'Integration Token'
-    string :channel, :required => true, :label => 'Channel to publish'
+    string :token, :required => true, :hint => 'Slack API Token'
+    string :channel, :required => true, :label => 'Channel Name'
     string :name, :required => true, :label => 'Publisher Name'
+    boolean :notify_ticket_created, :default => true, :label => 'Notify when Ticket is created'
+    boolean :notify_customer_reply_created, :default => true, :label => "Notify when a customer replied"
+    boolean :notify_agent_reply_created, :default => true, :label => "Notify when an agent replies"
+    boolean :notify_comment_created, :default => true, :label => "Notify when a comment is posted"
+
+    white_list :notify_ticket_created, :notify_agent_reply_created, :notify_customer_reply_created, :notify_comment_created
+
 
     private   
 
@@ -53,7 +67,12 @@ module Slack
     def post_reply(reply,ticket)      
       text = "RE: #{ticket.subject} from #{reply.replier.name} (#{reply.replier.email})"
       text += reply.content.text
-      post_to_slack(text);     
+      post_to_slack(text)     
+    end
+
+    def post_comment(comment)
+      text = comment.content.text
+      post_to_slack(text)
     end
 
      def post_to_slack(text)
