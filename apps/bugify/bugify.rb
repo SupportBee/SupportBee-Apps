df26b9e9aa3969a57aeb61e213d49091e9b25913
Prateek Dayal <prateek@supportbee.com>
2014-05-02 16:59:30 +0530

    Bugify app with multiple project support

--- apps/bugify/bugify.rb
+++ apps/bugify/bugify.rb
@@ -11,6 +11,11 @@ module Bugify
       end
       [200, "Ticket sent to Bugify"]
     end
+    
+    def projects
+      [200, fetch_projects]
+    end
+  
   end
 end
 
@@ -18,24 +23,50 @@ module Bugify
   require 'json'
 
   class Base < SupportBeeApp::Base
-    string :domain, :required => true, :label => 'Bugify domain (eg https://bugify.me.com/)'
+    string :url, :required => true, :label => 'Bugify url (eg https://bugify.me.com/)'
     string :api_key, :required => true, :label => 'Bugify API Key'
 
     def validate
-      errors[:flash] = ["Please fill in all the required fields"] if settings.api_key.blank? or settings.domain.blank?
+      errors[:flash] = ["Please fill in all the required fields"] if settings.api_key.blank? or settings.url.blank?
+      errors.empty? ? true : false
+    end
+    
+    def validate
+      errors[:flash] = ["URL or API Key Incorrect"] unless test_ping.success?
       errors.empty? ? true : false
     end
 
     private
 
+    def test_ping
+      response = http.get api_url('projects')
+    end
+    
+    def fetch_projects
+      response = bugify_get(api_url('projects'))['projects'].to_json
+    end
+
     def create_issue(subject, description)
-      response = http_post "#{settings.domain}api/issues.json?api_key=#{settings.api_key}" do |req|
-        req.body = URI.encode_www_form({:subject => subject, :description => description})
+      response = http_post api_url('issues') do |req|
+        req.body = URI.encode_www_form(:project => project_id,:subject => subject,:description => description.gsub('\n', '<br/>'))
       end
     end
 
+    def bugify_get(url)
+      response = http.get "#{url.to_s}"
+      response.body
+    end
+    
+    def project_id
+      payload.overlay.projects_select
+    end
+
+    def api_url(resource="")
+      "#{settings.url}/api/#{resource}.json?api_key=#{settings.api_key}"
+    end
+
     def comment_html(response)
-      "<a href=#{settings.domain}/issues/#{response.body['issue_id']}>Bugify issue #{response.body['issue_id']} created</a>"
+      "<a href=#{settings.url}/issues/#{response.body['issue_id']}>Bugify issue #{response.body['issue_id']} created</a>"
     end
 
     def comment_on_ticket(ticket, html)
