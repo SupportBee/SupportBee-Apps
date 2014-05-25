d1b3398533231d3adfc239e567f88d79ed3319be
Pranav Singh <developer.pranav@gmail.com>
2014-05-25 21:32:07 +0530

    notifications sent as attachments to Slack

--- apps/slack/slack.rb
+++ apps/slack/slack.rb
@@ -21,10 +21,10 @@ module Slack
     end
 
     def comment_created
-      return unless settings.notify_comment_created.to_s == '1'      
+      return unless settings.notify_comment_created.to_s == '1'
       post_comment(payload.comment, payload.ticket)
-      true      
-    end    
+      true
+    end
 
   end
 
@@ -34,7 +34,7 @@ module Slack
 
   module ActionHandler
 
-    def button     
+    def button
      [200, "Success"]
     end
 
@@ -47,40 +47,53 @@ module Slack
   require 'json'
 
   class Base < SupportBeeApp::Base
-     
-    string :token, :required => true, :hint => 'Slack API Token'
-    string :channel, :required => true, :label => 'Channel Name', :hint => "If #example is the Channel you want to send messages to, then enter 'example'"
-    string :name, :required => true, :label => 'Publisher Name'
-    string :domain, :required => true, :label => 'Company Name in Domain', :hint => 'If your base URL is "http://example.slack.com", then enter "example"'
+
+    string :webhook_url, :required => true, :label => 'Webhook URL'
     boolean :notify_ticket_created, :default => true, :label => 'Notify when Ticket is created'
     boolean :notify_customer_reply_created, :default => true, :label => "Notify when a customer replied"
     boolean :notify_agent_reply_created, :default => true, :label => "Notify when an agent replies"
     boolean :notify_comment_created, :default => true, :label => "Notify when a comment is posted"
     boolean :post_content, :default => false, :label => "Post Full Content to Slack"
 
-    white_list  :notify_ticket_created, 
-                :notify_agent_reply_created, 
-                :notify_customer_reply_created, 
+    white_list  :notify_ticket_created,
+                :notify_agent_reply_created,
+                :notify_customer_reply_created,
                 :notify_comment_created,
                 :post_content
 
 
-  private   
+  private
 
     def post_ticket(ticket)
-      text = "*New Ticket* from #{ticket.requester.name}: <https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}|#{ticket.subject}>"
-      if settings.post_content.to_s == '1'
-        text += "\n#{ticket.content.text}"
-      end
-      post_to_slack(text)
+      payload = {
+      	:username => "SupportBee",
+        :attachments => [
+          		:fallback => "New Ticket from #{ticket.requester.name} in <https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}|#{ticket.subject}>",
+        		:text => "New Ticket from #{ticket.requester.name} in <https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}|#{ticket.subject}>",
+          		:color => "danger",
+          		:fields => [
+          			:title => "#{ticket.subject}",
+          			:value => "#{ticket.content.text}"
+          		]
+          	]
+      }.to_json
+      post_to_slack(payload)
     end
 
     def post_agent_reply(reply, ticket)
-      text = "*Agent Reply* from #{reply.replier.name} in <https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}|#{ticket.subject}>"
-      if settings.post_content.to_s == '1'
-        text += "\n#{reply.content.text}"
-      end
-      post_to_slack(text)
+      payload = {
+      	:username => "SupportBee",
+        :attachments => [
+          		:fallback => "Agent Reply from #{reply.replier.name} in <https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}|#{ticket.subject}>",
+        		:text => "Agent Reply from #{reply.replier.name} in <https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}|#{ticket.subject}>",
+          		:color => "good",
+          		:fields => [
+          			:title => "Reply:",
+          			:value => "#{reply.content.text}"
+          		]
+          	]
+      }.to_json
+      post_to_slack(payload)
     end
 
     def post_customer_reply(reply, ticket)
@@ -92,16 +105,23 @@ module Slack
     end
 
     def post_comment(comment, ticket)
-      text = "*Comment* from #{comment.commenter.name} on <https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}|#{ticket.subject}>"
-      if settings.post_content.to_s == '1'
-        text += "\n#{comment.content.text}"
-      end
-      post_to_slack(text)
+      payload = {
+      	:username => "SupportBee",
+        :attachments => [
+          		:fallback => "#{comment.commenter.name} commented on <https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}|#{ticket.subject}>",
+        		:text => "#{comment.commenter.name} commented on <https://#{auth.subdomain}.supportbee.com/tickets/#{ticket.id}|#{ticket.subject}>",
+          		:color => "good",
+          		:fields => [
+          			:title => "Comment",
+          			:value => "#{comment.content.text}"
+          		]
+          	]
+      }.to_json
+      post_to_slack(payload)
     end
 
-    def post_to_slack(text)
-      payload = {"channel" => "##{settings.channel}", "username" => settings.name, "text" => text}.to_json
-      response = http_post "https://#{settings.domain}.slack.com/services/hooks/incoming-webhook?token=#{settings.token}" do |req|
+    def post_to_slack(payload)
+      response = http_post settings.webhook_url do |req|
       req.body = payload
     end
   end
