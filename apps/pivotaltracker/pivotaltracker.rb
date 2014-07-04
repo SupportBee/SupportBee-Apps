fda33d630a54627e310e9ce4887a1b4a7117a50a
Pranav Singh <developer.pranav@gmail.com>
2014-07-05 01:24:21 +0530

    added support for v5 of the api

--- apps/pivotaltracker/pivotaltracker.rb
+++ apps/pivotaltracker/pivotaltracker.rb
@@ -2,46 +2,95 @@ module Pivotaltracker
   module ActionHandler
     def button
       ticket = payload.tickets.first
-      begin
-       response =  create_story(payload.overlay.title, payload.overlay.description) 
-       return [500, "Unauthorized. Please check the Project ID and Token"] unless response
+      story = create_story(payload.overlay.title, payload.overlay.description)
+      return [500, "Story not sent!"] unless story
+      html = story_info_html(story)
 
-       html = comment_html(response, payload.overlay.title)
-       comment_on_ticket(ticket, html)
-      rescue Exception => e
-        return [500, e.message]
-      end
-      
-      [200, "Ticket sent to Pivotal Tracker"]
+      [200, "Success"]
+    end
+
+    def projects
+      [200, fetch_projects]
     end
   end
 end
 
-
-
 module Pivotaltracker
+  require 'json'
+
   class Base < SupportBeeApp::Base
-    string :token, :required => true, :label => 'Token'
-    string :project_id, :required => true, :label => 'Project ID'
+    string :token, required: true, label: 'Token'
+
+    def validate
+      errors[:flash] = ["Please fill in the Pivotal API Token"] if validate_presence_of_token
+      errors.empty? ? true : false
+    end
+
+    def validate
+      errors[:flash] = ["There seems to problem with your API Token"] unless test_ping.success?
+      errors.empty? ? true : false
+    end
+
+    def project_id
+      # Will return the ID of the selected project from the overlay form
+      "1114382"
+    end
 
     private
 
-    def create_story(story_name, description)
-      response = http_post "https://www.pivotaltracker.com/services/v3/projects/#{settings.project_id}/stories" do |req|
+    def test_ping
+      response = http_get projects_url do |req|
         req.headers['X-TrackerToken'] = settings.token
-        req.headers['Content-Type'] = 'application/xml'
-        req.body = "<story><story_type>feature</story_type><name><![CDATA[#{story_name}]]></name><description><![CDATA[#{description}]]></description></story>"
+        req.headers['Content-Type'] = 'application/json'
+      end
+      response
+    end
+
+    def validate_presence_of_token
+      not(settings.token.blank?)
+    end
+
+    def create_story(story_name, description)
+      response = http_post stories_url do |req|
+        req.headers['X-TrackerToken'] = settings.token
+        req.headers['Content-Type'] = 'application/json'
+        req.body = {
+          name: story_name,
+          description: description,
+          labels: [{
+            name: 'supportbee'
+          }]
+        }.to_json
       end
       response if response.status == 200
     end
-    
-    def comment_html(response, title)
-      hash_response = Hash.from_xml(response.body)
-      "Pivotal Tracker Story Created! \n <a href='https://www.pivotaltracker.com/s/projects/#{hash_response['story']['project_id']}/stories/#{hash_response['story']['id']}'>#{title}</a>"
+
+    def fetch_projects
+      response = http_get projects_url do |req|
+        req.headers['X-TrackerToken'] = settings.token
+        req.headers['Content-Type'] = 'application/json'
+      end
+      response.body.to_json
     end
 
+    def projects_url
+      pivotal_url("projects") 
+    end
+
+    def stories_url
+      pivotal_url("projects/#{project_id}/stories")
+    end
+
+    def pivotal_url(resource="")
+      "https://www.pivotaltracker.com/services/v5/#{resource}"
+    end
+
+    def story_info_html(story)
+      "Pivotal Tracker Story Created! \n <a href='#{story.body['url']}>#{story.body['name']}</a>"
+    end
+    
     def comment_on_ticket(ticket, html)
-       ticket.comment(:html => html)
+      ticket.comment(html: html)
     end
   end
 end
