6b4ddd62b878cb0343cc0038dd78afd409e09ae0
Swaroop SM <swaroop.striker@gmail.com>
2014-12-08 09:02:18 +0530

    added logic to talk to mailchimp api and also removed mailchimp gem

--- Gemfile
+++ Gemfile
@@ -33,7 +33,6 @@ gem 'bigcommerce', "~> 0.8.2"
 gem 'rubyzoho'
 gem 'restforce'
 gem 'mab'
-gem 'mailchimp'
 
 gem 'execjs', :group => :development
 gem 'pry', group: :development
--- apps/mailchimp/mailchimp.rb
+++ apps/mailchimp/mailchimp.rb
@@ -1,39 +1,91 @@
 module Mailchimp
-  module EventHandler
-    # Handle 'ticket.created' event
-    def ticket_created
-      return true
-    end
-
-    # Handle all events
-    def all_events
-      return true
-    end
-  end
-end
-
-module Mailchimp
   module ActionHandler
     def button
-     # Handle Action here
-     [200, "Success"]
+      ticket = payload.tickets.first
+      email = ticket.requester.email
+
+      begin
+        add_to_list(ticket)
+      rescue Mailchimp::ListAlreadySubscribedError
+        return [ 200, "#{email} is already subscribed to the list." ]
+      rescue Mailchimp::ListDoesNotExistError
+        return [ 200, "This list could not be found." ]
+      rescue Excpetion => e
+        return [ 500, e.message ]
+      end
+
+      [200, "Success"]
+    end
+
+    def lists
+      begin
+        all_lists = get_lists
+
+        return [ 200, all_lists ]
+      rescue Exception => e
+        return [ 500, e.message ]
+      end
     end
   end
 end
 
 module Mailchimp
   class Base < SupportBeeApp::Base
+    
     # Define Settings
-    # string :name, :required => true, :hint => 'Tell me your name'
-    # string :username, :required => true, :label => 'User Name'
-    # password :password, :required => true
-    # boolean :notify_me, :default => true, :label => 'Notify Me'
+    string :api_key, :required => true, :hint => 'Get you API key at: http://kb.mailchimp.com/article/where-can-i-find-my-api-key'
 
     # White list settings for logging
-    # white_list :name, :username
+    white_list :api_key
 
-    # Define public and private methods here which will be available
-    # in the EventHandler and ActionHandler modules
+    def get_lists
+      response = http_post api_url("/lists/list.json") do |req|
+        req.body = { 'apikey' => settings.api_key }.to_json
+        req.headers['Content-Type'] = 'application/json'
+      end
+
+      response_body = response.body
+
+      if response_body['data']
+        return response_body['data']
+      end
+
+      raise response_body['error']
+    end
+
+    def list_id
+      payload.overlay.lists_select
+    end
+
+    private
+    def api_url(path)
+      splitted = settings.api_key.to_s.split("-")
+      version = "2.0"
+
+      if splitted.length == 2
+        ds = splitted[1]
+        return "https://#{ds}.api.mailchimp.com/#{version}#{path}"
+      end
+
+      raise "Invalid API key format"
+
+    end
+
+    def add_to_list(ticket)
+      response = http_post api_url("/lists/subscribe") do |req|
+        req.body = { 'apikey' => settings.api_key, 'id' => list_id, 'email' => ticket.requester.email, 'name' => ticket.requester.name }.to_json
+        req.headers['Content-Type'] = 'application/json'
+      end
+
+      response_body = response.body
+
+      if response_body['data']
+        return response_body['data']
+      end
+
+      raise response_body['error']
+    end
+
   end
 end
 
