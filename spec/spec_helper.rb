286be2ac444c37f9e55d1ab8554d414c8daa9749
Mohnish G J <mail4mohnish@gmail.com>
2014-12-24 13:14:48 +0530

    using the latest rspec version for testing and adding some addn conifg options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
 ENV['RACK_ENV'] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/load")
 
+module RackSpecHelpers
+  include Rack::Test::Methods
+  def app
+    Sinatra::Application
+  end
+end
+
 RSpec.configure do |conf|
-  conf.include Rack::Test::Methods
+  conf.include RackSpecHelpers
   conf.mock_with :flexmock
 end
