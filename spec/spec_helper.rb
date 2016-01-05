8fc1aafd9e656f73c1904c132e12bdc6d95bd840
Nisanth Chunduru <nisanth074@gmail.com>
2016-01-05 15:00:57 +0530

    Fix whitespace in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,13 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/load")
 
 module RackSpecHelpers
   include Rack::Test::Methods
+
   def app
     RunApp
   end
 end
 
-RSpec.configure do |conf|
-  conf.include RackSpecHelpers
-  conf.mock_with :flexmock
+RSpec.configure do |config|
+  config.include RackSpecHelpers
+  config.mock_with :flexmock
 end
