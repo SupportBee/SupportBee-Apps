b493adec52a009bc5d827090a1c9b8c635113b21
Paulo <phsoares.ita@gmail.com>
2016-05-19 16:54:21 -0300

    Webhooks working as a proxy for requests

--- apps/webhook/webhook.rb
+++ apps/webhook/webhook.rb
@@ -1,23 +1,26 @@
 module Webhook
   module EventHandler
-    def ticket_created
-      return super unless defined?(payload) && payload
+    def method_missing(method, *args)
       webhook_urls = settings.urls.split(/\s*,\s*/)
       webhook_urls.each do |url|
         post(url.strip, {payload: payload}.to_json)
       end
-      true
+    end
+
+    def respond_to?(method)
+      method_name_is_in_underscore_format = method =~ /^[\w_]+$/
+      method_name_is_in_underscore_format && defined?(payload)
     end
   end
 end
 
 module Webhook
   class Base < SupportBeeApp::Base
-    string :urls, :required => true, :hint => 'If you need multiple URLs separate them with a comma (E.g.: http://example1.com, http://example2.com)'
-    white_list :urls
+    string :urls, 
+           :required => true,
+           :hint => 'If you need multiple URLs separate them with a comma (E.g.: http://example1.com, http://example2.com)'
 
-    # Define public and private methods here which will be available
-    # in the EventHandler and ActionHandler modules
+    white_list :urls
 
     def post(url, payload)
       connection = Faraday.new(url: url)
