fa8583dd53b5ca9f9a55952e68c36b043cc0fed2
Paulo <phsoares.ita@gmail.com>
2016-05-20 09:15:33 -0300

    responding to all events

--- apps/webhook/webhook.rb
+++ apps/webhook/webhook.rb
@@ -1,22 +1,21 @@
+require_relative '../../lib/app_mixins/proxy_event_handler'
+
 module Webhook
   module EventHandler
-    def method_missing(method, *args)
+    include SupportBee::ProxyEventHandler
+
+    def all_events
       webhook_urls = settings.urls.split(/\s*,\s*/)
       webhook_urls.each do |url|
         post(url.strip, {payload: payload}.to_json)
       end
-    end
-
-    def respond_to?(method)
-      method_name_is_in_underscore_format = method =~ /^[\w_]+$/
-      method_name_is_in_underscore_format && defined?(payload)
     end
   end
 end
 
 module Webhook
   class Base < SupportBeeApp::Base
-    string :urls, 
+    string :urls,
            :required => true,
            :hint => 'If you need multiple URLs separate them with a comma (E.g.: http://example1.com, http://example2.com)'
 
--- spec/apps/webhook/webhook_spec.rb
+++ spec/apps/webhook/webhook_spec.rb
@@ -3,14 +3,40 @@ require 'yaml'
 
 describe Webhook do
   let(:ticket_created) { YAML.load_file('spec/fixtures/webhook/ticket_created.yml')}
+  let(:ticket_trashed) { YAML.load_file('spec/fixtures/webhook/ticket_trashed.yml')}
+
   let(:url1) { "http://example1.com" }
   let(:url2) { "http://example2.com" }
 
-  describe "create to-do item" do
+  describe "work as proxy for all events" do
+    context ".api_hash" do
+      it "should return a list containing 'all.events'" do
+        Webhook::Base.api_hash['events'].should == ['all.events']
+      end
+    end
+  end
+
+  describe "ticket created" do
     it "sends correct data to the expected urls" do
-      stub_request(:post, url1).to_return(status: 200, body: "", headers: {'Content-Type' => 'application/json'})
-      stub_request(:post, url2).to_return(status: 200, body: "", headers: {'Content-Type' => 'application/json'})
+      request_params = {body: ticket_created[:payload]}
+      response_params = {status: 200, body: "", headers: {'Content-Type' => 'application/json'}}
+
+      stub_request(:post, url1).with(request_params).to_return(response_params)
+      stub_request(:post, url2).with(request_params).to_return(response_params)
+
       response = post "/webhook/event/ticket.created", ticket_created.to_json
+      response.status.should == 204
+    end
+  end
+
+  describe "ticket trashed" do
+    it "sends correct data to the expected urls" do
+      request_params = {body: ticket_trashed[:payload]}
+      response_params = {status: 200, body: "", headers: {'Content-Type' => 'application/json'}}
+
+      stub_request(:post, url1).with(request_params).to_return(response_params)
+
+      response = post "/webhook/event/ticket.trashed", ticket_trashed.to_json
       response.status.should == 204
     end
   end
