6eede4502c2bf466864b2aec2273379c121deb12
Nisanth Chunduru <nisanth074@gmail.com>
2017-03-27 01:42:31 +0530

    Cleanup config/load.rb

--- config/load.rb
+++ config/load.rb
@@ -4,14 +4,9 @@ PLATFORM_ROOT = File.expand_path '../../', __FILE__
 require 'bundler'
 Bundler.setup
 
+# Require gems in Gemfile
 Bundler.require(:default, PLATFORM_ENV.to_sym)
-
-# Setup newrelic monitoring
-if ['staging', 'production'].include?(PLATFORM_ENV)
-  require 'newrelic_rpm'
-end
-
-require "#{PLATFORM_ROOT}/config/environments/#{PLATFORM_ENV}"
+require 'error-reporter'
 
 require 'active_support/core_ext/string/inflections'
 require 'active_support/core_ext/object/blank'
@@ -21,25 +16,24 @@ Dir["#{PLATFORM_ROOT}/lib/*.rb"].each { |f| require f }
 Dir["#{PLATFORM_ROOT}/apps/*/*.rb"].each { |f| require f }
 Dir["#{PLATFORM_ROOT}/workers/*.rb"].each { |f| require f }
 
-app_config = YAML.load_file("#{PLATFORM_ROOT}/config/sba_config.yml")[PLATFORM_ENV]['app_platform']
-SECRET_CONFIG = YAML.load_file("#{PLATFORM_ROOT}/config/secret_config.yml")[PLATFORM_ENV]
-OMNIAUTH_CONFIG = YAML.load_file("#{PLATFORM_ROOT}/config/omniauth.platform.yml")[PLATFORM_ENV]
-
-# Override the config of core app to local server if super user
-if ENV['SU']
-  app_config['core_app_base_domain'] = 'lvh.me'
-  app_config['core_app_port'] = 3000
-  app_config['core_app_protocol'] =  'http'
+# Setup newrelic monitoring
+if ['staging', 'production'].include?(PLATFORM_ENV)
+  require 'newrelic_rpm'
 end
 
-APP_CONFIG = app_config
+APP_CONFIG = YAML.load_file("#{PLATFORM_ROOT}/config/sba_config.yml")[PLATFORM_ENV]['app_platform']
+SECRET_CONFIG = YAML.load_file("#{PLATFORM_ROOT}/config/secret_config.yml")[PLATFORM_ENV]
+OMNIAUTH_CONFIG = YAML.load_file("#{PLATFORM_ROOT}/config/omniauth.platform.yml")[PLATFORM_ENV]
+APPS_PATH = "#{PLATFORM_ROOT}/apps"
 
+# Setup logging
 log_dir = "#{PLATFORM_ROOT}/log"
 log_filename = "#{PLATFORM_ENV}.log"
 log_url = "#{log_dir}/#{log_filename}"
 FileUtils.mkdir(log_dir) unless File.exists?(log_dir)
 LOGGER = Logger.new(log_url)
 
+# Setup redis
 REDIS_CONFIG = APP_CONFIG['redis']
 redis = nil
 if PLATFORM_ENV == 'test'
@@ -51,17 +45,7 @@ else
 end
 REDIS = redis
 
-if PLATFORM_ENV == 'development'
-  require 'pry'
-end
-
-require 'error-reporter'
-require "#{PLATFORM_ROOT}/run_app"
-
-#
-# Sidekiq
-#
-
+# Setup sidekiq
 SIDEKIQ_REDIS_CONFIG = APP_CONFIG['sidekiq_redis']
 default_redis_port = 6379
 port = SIDEKIQ_REDIS_CONFIG['port'] || default_redis_port
@@ -72,3 +56,5 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: redis_url }
 end
+
+require "#{PLATFORM_ROOT}/run_app"
