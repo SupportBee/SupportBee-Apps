59457c5c7b5d7ff408e999da94e8177c09e3963b
Nisanth Chunduru <nisanth074@gmail.com>
2017-03-28 00:28:51 +0530

    Send failed sidekiq jobs to the Dead queue

--- workers/trigger_app_event.rb
+++ workers/trigger_app_event.rb
@@ -1,6 +1,8 @@
 class TriggerAppEvent
   include Sidekiq::Worker
 
+  sidekiq_options retry: 0
+
   def perform(app_slug, event, data, payload)
     app_class = SupportBeeApp::Base.find_from_slug(app_slug)
     app_class.trigger_event(event, data, payload)
